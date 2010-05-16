3425d6daabbe7d3694353af5e31c6d435f89a21f
Gerad Suyderhoud <gerads@gmail.com>
2010-05-15 20:32:09 -0700

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 (function(){
+  var server, sio;
   require.paths.unshift('./lib/express/lib');
   require('express');
+  // required  for views
+  configure(function() {
+    return set('root', __dirname);
+  });
   get('/', function() {
-    return this.redirect('/hello/world');
+    return this.render('index.html.haml');
   });
-  get('/hello/world', function() {
-    return 'Hello World';
+  server = run(parseInt(process.env.PORT || 8000), null);
+  // handle web sockets
+  sio = require('./lib/socket.io/lib/socket.io');
+  sio.listen(server, {
+    resource: '/ws',
+    transports: 'websocket htmlfile xhr-multipart xhr-polling'.split(' ')
   });
-  run(parseInt(process.env.PORT || 8000), null);
 })();
