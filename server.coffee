1ab88c7362c88fbbd07231171779f6b5b11fd0f0
Gerad Suyderhoud <gerads@gmail.com>
2010-05-17 09:38:25 -0700

    try to get sockets working

--- server.coffee
+++ server.coffee
@@ -1,5 +1,9 @@
 require.paths.unshift './lib/express/lib'
 require 'express'
+require 'express/plugins'
+use Static # require for public
+use Logger
+json: JSON.stringify
 
 # required  for views
 configure ->
@@ -12,7 +16,13 @@ server: run parseInt(process.env.PORT || 8000), null
 
 # handle web sockets
 sio: require './lib/socket.io/lib/socket.io'
-sio.listen server, {
-  resource: '/ws',
+commet: sio.listen server, {
+  resource: 'commet'
   transports: 'websocket htmlfile xhr-multipart xhr-polling'.split(' ')
+  onClientConnect: (client) ->
+    commet.broadcast json({ msg: "${client.sessionId} connected" })
+  onClientDisconnect: (client) ->
+    client.broadcast json({ msg: "${client.sessionId} disconnected" })
+  onClientMessage: (message, client) ->
+    commet.broadcast json({ msg: "${client.sessionId} wrote $message" })
 }
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 (function(){
-  var server, sio;
+  var commet, json, server, sio;
   require.paths.unshift('./lib/express/lib');
   require('express');
+  require('express/plugins');
+  use(Static);
+  // require for public
+  use(Logger);
+  json = JSON.stringify;
   // required  for views
   configure(function() {
     return set('root', __dirname);
@@ -12,8 +17,23 @@
   server = run(parseInt(process.env.PORT || 8000), null);
   // handle web sockets
   sio = require('./lib/socket.io/lib/socket.io');
-  sio.listen(server, {
-    resource: '/ws',
-    transports: 'websocket htmlfile xhr-multipart xhr-polling'.split(' ')
+  commet = sio.listen(server, {
+    resource: 'commet',
+    transports: 'websocket htmlfile xhr-multipart xhr-polling'.split(' '),
+    onClientConnect: function onClientConnect(client) {
+      return commet.broadcast(json({
+        msg: ("" + (client.sessionId) + " connected")
+      }));
+    },
+    onClientDisconnect: function onClientDisconnect(client) {
+      return client.broadcast(json({
+        msg: ("" + (client.sessionId) + " disconnected")
+      }));
+    },
+    onClientMessage: function onClientMessage(message, client) {
+      return commet.broadcast(json({
+        msg: ("" + (client.sessionId) + " wrote " + message)
+      }));
+    }
   });
 })();
